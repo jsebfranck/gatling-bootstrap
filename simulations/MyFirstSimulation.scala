06dcc2bee0e81587240edc6dfb95f244cd52f183
Jean-Sébastien Franck <jsebfranck@gmail.com>
2014-09-19 14:54:48 +0200

    Migrated to gatling 2.0.0-RC5 release

--- simulations/MyFirstSimulation.scala
+++ simulations/MyFirstSimulation.scala
@@ -2,11 +2,7 @@ import io.gatling.core.Predef._
 import io.gatling.core.session.Expression
 import io.gatling.http.Predef._
 import io.gatling.jdbc.Predef._
-import io.gatling.http.Headers.Names._
-import io.gatling.http.Headers.Values._
 import scala.concurrent.duration._
-import bootstrap._
-import assertions._
 
 class MyFirstSimulation extends Simulation {
 
@@ -15,5 +11,5 @@ class MyFirstSimulation extends Simulation {
 	val scn = scenario("Global scenario")
         .exec(http("GET google home").get("/"))
 
-	setUp(scn.inject(atOnce(1 user))).protocols(httpProtocol)
+	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
 }
